391fa4ecfec3dd89afcff342c266dd6198bf44da
Hasitha-Nilwakka <hasitha.nilwakka@yahoo.com>
2026-04-21 15:51:53 +0300

    added the basic shell for the design

---
+++ client/src/components/JobDescription.tsx
@@ -0,0 +1,7 @@
+export default function JobDescription() {
+    return (
+        <div className="w-[45%] bg-gray-200">
+            <h1>Job Description</h1>
+        </div>
+    )
+}
---
+++ client/src/components/ResumeSection.tsx
@@ -0,0 +1,7 @@
+export default function ResumeSection() {
+    return (
+        <div className="w-[45%] bg-gray-200">
+            <h1>Resume Section</h1>
+        </div>
+    )
+}
